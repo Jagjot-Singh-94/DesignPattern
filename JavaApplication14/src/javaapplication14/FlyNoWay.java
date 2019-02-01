32540ba113c6d4746fc5d46e4affe0441c4d4c70
Jagjot-Singh-94 <43452497+Jagjot-Singh-94@users.noreply.github.com>
2019-02-01 13:28:24 +0530

    Add files via upload

---
+++ JavaApplication14/src/javaapplication14/FlyNoWay.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication14;
+
+/**
+ *
+ * @author jagjosin
+ */
+public class FlyNoWay implements FlyBehaviour{
+
+    @Override
+    public void fly() {
+        System.out.println("Do not fly");
+    }
+    
+    
+}
---
+++ JavaApplication14/src/javaapplication14/Squeak.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication14;
+
+/**
+ *
+ * @author jagjosin
+ */
+public class Squeak implements QuackBehaviour {
+    @Override
+    public void quack(){
+        System.out.println("Squeak!!!");
+    }
+}
